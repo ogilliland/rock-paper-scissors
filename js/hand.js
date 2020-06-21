0d6c1befe77f6591ff63e29f42807a27102ec3bb
Oliver Gilliland <omgilliland@gmail.com>
2020-06-21 13:00:06 +0200

    Add skin to bones

--- js/hand.js
+++ js/hand.js
@@ -1,85 +1,92 @@
 function Hand(side) {
 	var self = this;
 	self.side = side;
+
 	if(side == "left") {
-		self.bones = [
-			// forearm
-			new Bone(-400, 400, 500, 0, [
-				// palm
-				new Bone(0, 0, 175, 0, [
-					finger(0, -75, 1.125),
-					finger(0, -25, 1.25),
-					finger(0, 25, 1.125),
-					finger(0, 75, 1),
-					// thumb first knuckle
-					new Bone(-150, -75, 75, -0.8, [
-						// thumb second knuckle
-						new Bone(0, 0, 75, 0.4, [
-							// thumb tip
-							new Bone(0, 0, 50, 0.2)
-						])
-					])
-				])
-			])
-		];
+		var factor = 1;
+		var offset = 0;
 	} else {
-		self.bones = [
-			// forearm
-			new Bone(window.innerWidth + 400, 400, -500, 0, [
-				// palm
-				new Bone(0, 0, -175, 0, [
-					finger(0, -75, -1.125),
-					finger(0, -25, -1.25),
-					finger(0, 25, -1.125),
-					finger(0, 75, -1),
-					// thumb first knuckle
-					new Bone(150, -75, -75, 0.8, [
-						// thumb second knuckle
-						new Bone(0, 0, -75, -0.4, [
-							// thumb tip
-							new Bone(0, 0, -50, -0.2)
-						])
+		var factor = -1;
+		var offset = window.innerWidth;
+	}
+
+	self.bones = [
+		// forearm
+		new Bone(-400*factor+offset, 400, 500*factor, 0, 50, [
+			// palm
+			new Bone(0, 0, 150*factor, 0, 75, [
+				finger(0, -60, 1.125*factor),
+				finger(0, -20, 1.25*factor),
+				finger(0, 20, 1.125*factor),
+				finger(0, 60, 1*factor),
+				// thumb first knuckle
+				new Bone(-150*factor, -50, 90*factor, -0.8*factor, 20, [
+					// thumb second knuckle
+					new Bone(0, 0, 50*factor, 0.4*factor, 17.5, [
+						// thumb tip
+						new Bone(0, 0, 50*factor, 0.2*factor, 17.5)
 					])
 				])
 			])
-		];
-	}
+		])
+	];
 
 	function finger(x, y, scale = 1) {
 		// first knuckle
-		return new Bone(x, y, 50*scale, 0, [
+		return new Bone(x, y, 45*scale, 0, 15, [
 			// second knuckle
-			new Bone(0, 0, 50*scale, 0, [
+			new Bone(0, 0, 45*scale, 0, 15, [
 				// finger tip
-				new Bone(0, 0, 40*scale, 0)
+				new Bone(0, 0, 35*scale, 0, 15)
 			])
 		])
 	}
 
 	self.draw = function(ctx) {
 
-		ctx.strokeStyle = "red";
-		ctx.fillStyle = "red";
 		ctx.lineWidth = 4;
 
-		function drawBone(bone, dx = 0, dy = 0, dangle = 0) {
+		function drawBone(bone, showSkin = true, showBone = false, dx = 0, dy = 0, dangle = 0) {
 			// modify bone.x and bone.y by remaining angle (dangle)
 			var x1 = dx + bone.x*Math.cos(dangle) - bone.y*Math.sin(dangle);
 			var y1 = dy + bone.y*Math.cos(dangle) + bone.x*Math.sin(dangle);
 			var x2 = dx + bone.x*Math.cos(dangle) - bone.y*Math.sin(dangle) + bone.length*Math.cos(bone.angle + dangle);
 			var y2 = dy + bone.y*Math.cos(dangle) + bone.x*Math.sin(dangle) + bone.length*Math.sin(bone.angle + dangle);
-			// draw bone
-			ctx.beginPath();
-			ctx.moveTo(x1, y1);
-			ctx.lineTo(x2, y2);
-			ctx.stroke();
-			// circle at bone origin
-			ctx.beginPath();
-			ctx.arc(x1, y1, 6, 0, 2*Math.PI);
-			ctx.fill();
+			if(showSkin) {
+				// calculate skin vertices
+				var sx1 = x1 - bone.skinWidth*Math.sin(dangle + bone.angle);
+				var sy1 = y1 + bone.skinWidth*Math.cos(dangle + bone.angle);
+				var sx2 = x1 + bone.skinWidth*Math.sin(dangle + bone.angle);
+				var sy2 = y1 - bone.skinWidth*Math.cos(dangle + bone.angle);
+				var sx3 = x2 + bone.skinWidth*Math.sin(dangle + bone.angle);
+				var sy3 = y2 - bone.skinWidth*Math.cos(dangle + bone.angle);
+				var sx4 = x2 - bone.skinWidth*Math.sin(dangle + bone.angle);
+				var sy4 = y2 + bone.skinWidth*Math.cos(dangle + bone.angle);
+				// draw skin
+				ctx.fillStyle = "#ffdbac";
+				ctx.beginPath();
+				ctx.moveTo(sx1, sy1);
+				ctx.lineTo(sx2, sy2);
+				ctx.lineTo(sx3, sy3);
+				ctx.lineTo(sx4, sy4);
+				ctx.fill();
+			}
+			if(showBone) {
+				// draw bone
+				ctx.strokeStyle = "red";
+				ctx.beginPath();
+				ctx.moveTo(x1, y1);
+				ctx.lineTo(x2, y2);
+				ctx.stroke();
+				// circle at bone origin
+				ctx.fillStyle = "red";
+				ctx.beginPath();
+				ctx.arc(x1, y1, 6, 0, 2*Math.PI);
+				ctx.fill();
+			}
 			// recursively draw children with offset from parent
 			for(var i = 0; i < bone.children.length; i++) {
-				drawBone(bone.children[i], x2, y2, bone.angle + dangle);
+				drawBone(bone.children[i], showSkin, showBone, x2, y2, bone.angle + dangle);
 			}
 		}
 
@@ -89,11 +96,12 @@ function Hand(side) {
 	}
 }
 
-function Bone(x, y, length, angle = 0, children = []) {
+function Bone(x, y, length, angle = 0, skinWidth = 50, children = []) {
 	var self = this;
 	self.x = x;
 	self.y = y;
 	self.length = length;
 	self.angle = angle;
 	self.children = children;
+	self.skinWidth = skinWidth;
 }
